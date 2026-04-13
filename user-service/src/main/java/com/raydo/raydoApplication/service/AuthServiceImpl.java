38d39772720e7a94e3582c2606157d6e1c99b5ed
Vatsalya Sharma <vatsalya.sharma@globallogic.com>
2026-04-13 10:03:33 +0530

    Upadte: renamed for consistency

--- user-service/src/main/java/com/raydo/raydoApplication/service/AuthServiceImpl.java
+++ user-service/src/main/java/com/raydo/raydoApplication/service/AuthServiceImpl.java
@@ -1,12 +1,20 @@
 package com.raydo.raydoApplication.service;
 
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 import com.raydo.raydoApplication.config.TokenUtil;
 import com.raydo.raydoApplication.dto.AuthResponseDTO;
 import com.raydo.raydoApplication.dto.UserResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
+import org.springframework.web.client.RestTemplate;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.List;
 import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.Random;
@@ -22,6 +30,41 @@ public class AuthServiceImpl implements AuthService {
 
     private static final int OTP_EXPIRY_MINUTES = 5;
 
+    private void sendOtpFromPhone(String phoneNumber, String otp) {
+
+        String url = "http://192.168.0.101:8080/message"; 
+        String username = "sms";
+        String password = "-WVC2yjn";
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        // 🔐 Basic Auth
+        String auth = username + ":" + password;
+        String encodedAuth = "Basic " + Base64.getEncoder().encodeToString(auth.getBytes());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization", encodedAuth);
+
+        HashMap<String, Object> textMessage = new HashMap<>();
+        textMessage.put("text", "Your OTP is: " + otp);
+
+        HashMap<String, Object> body = new HashMap<>();
+        body.put("textMessage", textMessage);
+        body.put("phoneNumbers", List.of(phoneNumber));
+
+        HttpEntity<HashMap<String, Object>> request = new HttpEntity<>(body, headers);
+
+        try {
+            ResponseEntity<String> response =
+                    restTemplate.postForEntity(url, request, String.class);
+
+            System.out.println("SMS Response: " + response.getBody());
+
+        } catch (Exception e) {
+            throw new RuntimeException("SMS sending failed", e);
+        }
+    }
     @Override
     public void requestOtp(String phoneNumber) {
 
@@ -31,7 +74,7 @@ public class AuthServiceImpl implements AuthService {
                 otp,
                 LocalDateTime.now().plusMinutes(OTP_EXPIRY_MINUTES)
         ));
-
+        sendOtpFromPhone(phoneNumber, otp);
         System.out.println("OTP for " + phoneNumber + " is: " + otp);
     }
 
